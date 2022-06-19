8682d954b731baa60bf4dc6eb8dc7d81d9862f52
dominic784 <dominic@Cenedex.com>
2022-06-18 20:35:24 -0400

    add questrade to resune

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
@@ -12,7 +14,7 @@ import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       foo: 'bar',
@@ -24,36 +26,57 @@ class App extends Component {
 
   }
 
-  getResumeData(){
+  getResumeData() {
     $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
+      url: '/resumeData.json',
+      dataType: 'json',
       cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
+      success: (data) =>
+        this.setState({
+          resumeData: data
+        }),
+      error: (xhr, status, err) => {
+        console.log(err)
       }
     });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getResumeData();
   }
 
   render() {
-    return (
-      <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      Header data = {
+        this.state.resumeData.main
+      }
+      /> <
+      About data = {
+        this.state.resumeData.main
+      }
+      /> <
+      Resume data = {
+        this.state.resumeData.resume
+      }
+      /> <
+      Portfolio data = {
+        this.state.resumeData.portfolio
+      }
+      /> <
+      Testimonials data = {
+        this.state.resumeData.testimonials
+      }
+      /> <
+      Contact data = {
+        this.state.resumeData.main
+      }
+      /> <
+      Footer data = {
+        this.state.resumeData.main
+      }
+      /> </div >
     );
   }
 }
